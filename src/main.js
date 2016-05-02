5510abcbbb603c20e9f115f1e3761dde79874b02
janekim0730 <janekim0730@gmail.com>
2016-05-02 23:27:08 +0000

    jQuery for NYT Api

--- src/main.js
+++ src/main.js
@@ -1,1 +1,43 @@
-console.log('Hellow World')
+$( document ).ready(function() { //load it when it is ready
+
+    $('.sections').on('change', function(event){ //whenever dropbox changes
+      event.preventDefault();
+
+      var $selection = $('.sections :selected').val(); //it stores the value after selected
+
+      $.ajax({//get Api
+        method: 'GET',
+        dataType: 'json',
+        url: 'http://api.nytimes.com/svc/topstories/v1/' + $selection + '.json?api-key=c37f75561abe3523181a126c4cb4dc92:12:75124075'})
+
+        .done(function(data){ //when it is successful grabing the json
+           console.log(data);
+
+           var nytData = data.results,
+                         articleLink,
+                         articleCaption,
+                         articleImageUrl;
+
+         //get populate only 12stories with images
+              nytData = nytData
+                        .filter(function(item){
+                            return item.multimedia.length;
+                          }).splice(0, 12);
+
+      var nytItems = " ";
+
+       for(var i=0; i<12; i++){
+            articleImageUrl = nytData[i].multimedia[4].url;
+            articleCaption = nytData[i].abstract;
+            articleLink = nytData[i].url;
+
+            nytItems += '<li class="article-item">';
+            nytItems += '<a class="articleimage" href="' + articleLink + '" target="_blank" style="background-image:url(\'' + articleImageUrl + '\');">';
+            nytItems += '<p class="caption">' + articleCaption + '</p></a></li>';
+          }
+
+          $('.item-container').append(nytItems);
+
+    }); //done function
+  }); // change function
+}); //ready function
