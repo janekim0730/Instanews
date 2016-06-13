21d85a9778eea19b22abfca67d8a4339a296fd21
janekim0730 <janekim0730@gmail.com>
2016-06-13 16:18:06 +0000

    uglify wasn't able to read es2015, so some of the codes have been modified

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,20 @@ var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
-gulp.task('default', ['uglify', 'browser-sync']);
 gulp.task('browser-sync', function() {
     browserSync.init({
         open: false,
         proxy: '192.168.33.10/project2'
     });
-
-    // Watch tasks
-    gulp.watch('./src/*.js', ['uglify']);
-    gulp.watch('./src/*.scss', ['sass']);
-
-    gulp.watch(['./build/**/*.*', 'index.html'])
-        .on('change', browserSync.reload);
 });
+    
+// Watch tasks
+gulp.watch('./src/*.js', ['uglify']);
+gulp.watch('./src/*.scss', ['sass']);
+
+gulp.watch(['./build/**/*.*', 'index.html'])
+        .on('change', browserSync.reload);
+
 gulp.task('sass', function(){
    return gulp.src('./src/*.scss')
               .pipe(sass().on('error', sass.logError))
@@ -25,5 +25,10 @@ gulp.task('sass', function(){
 gulp.task('uglify', function(){
    return gulp.src('./src/*.js') // What files do we want gulp to consume?
         .pipe(uglify()) // Call the uglify function on these files
+        .on('error', function(e){
+            console.log(e);
+         })
         .pipe(gulp.dest('./build')) // Where do we put the result?
 });
+
+gulp.task('default', ['uglify', 'sass', 'browser-sync']);
---
+++ src/es.js
@@ -0,0 +1,48 @@
+$( document ).ready(function() { //load it when it is ready
+
+    $('.sections').on('change', function(event){ //whenever dropbox changes
+      event.preventDefault();
+      $('.item-container').empty();
+      $('.categories').prepend('<p class="ajax-loader"><img src="images/ajax-loader.gif" alt="loading"></p> ');
+      $('.initial').addClass("minimize");
+      var $selection = $('.sections:selected').val(); //it stores the value after selected
+
+      $.ajax({//get Api
+        method: 'GET',
+        dataType: 'json',
+        url: 'http://api.nytimes.com/svc/topstories/v1/' + $selection + '.json?api-key=c37f75561abe3523181a126c4cb4dc92:12:75124075'})
+
+        .done(function(data){ //when it is successful grabing the json
+           console.log(data);
+
+           $('.ajax-loader').remove();
+
+           var nytData = data.results,
+                         articleLink,
+                         articleCaption,
+                         articleImageUrl;
+
+         //get populate only 12stories with images
+              nytData = nytData
+                        .filter(function(item){
+                            return item.multimedia.length;
+                          }).splice(0, 12);
+
+      var nytItems = " ";
+
+      var i = 0;
+       for(i; i<12; i++){
+            articleImageUrl = nytData[i].multimedia[4].url;
+            articleCaption = nytData[i].abstract;
+            articleLink = nytData[i].url;
+
+            nytItems += '<li class="article-item">';
+            nytItems += '<a class="articleimage" href="' + articleLink + '" target="_blank" style="background-image:url(\'' + articleImageUrl + '\');">';
+            nytItems += '<p class="text-container">' + articleCaption + '</p></a></li>';
+          }
+
+          $('.item-container').append(nytItems);
+
+    }); //done function
+  }); // change function
+}); //ready function
